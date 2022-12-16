1586840e01cecaf242d1d6de8a545a6358f3dc6a
Zeshan Haider <zeshan.haider@emumba.com>
2022-12-16 17:58:46 -0600

    The Redux is Almost ready

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const Navbar = () => {
+  const amount = useSelector((state) => state.amount);
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -35,7 +37,7 @@ export const Navbar = () => {
           </div>
           <div>
             <button disabled={true} className="btn btn-primary">
-              Your Balance{" "}
+              Your Balance: {amount}
             </button>
           </div>
         </div>
--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,7 @@
-import { createStoreHook } from "react-redux";
-import { applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk"; // it is used to help execute async functions.
 import reducers from "./reducers"
 
-export const store = createStoreHook(reducers, {}, applyMiddleware(thunk))
+export const store = createStore(reducers, {}, applyMiddleware(thunk))
 
 // To give access of this store to the whole application, I'll use 'Provider' in the index.js (main entry file )
