c183764e13a59b513ffce5266ea3d3576e8aaf1c
Zeshan Haider <zeshan.haider@emumba.com>
2022-12-16 15:50:47 -0600

    Basic setup for Redex

---
+++ src/Shop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Shop = () => {
+  return (
+    <>
+      <div className="container my-3">
+        <h2>Wanna Buy Cat Shoes!</h2>
+        <button className="btn btn-primary mx-2">+</button>
+        Add to Cart
+        <button className="btn btn-primary mx-2">-</button>
+      </div>
+    </>
+  );
+};
