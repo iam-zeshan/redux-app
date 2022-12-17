29d8373c5f609f98840b49e9e67ede7238f411dc
Zeshan Haider <zeshan.haider@emumba.com>
2022-12-16 18:10:07 -0600

    Using actionCreators without bindActionCreators

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { actionCreators } from "./state/index";
+// import { bindActionCreators } from "redux";
 
 export const Shop = () => {
+  const dispatch = useDispatch();
+
   return (
     <>
       <div className="container my-3">
         <h2>Deposit/Withdraw</h2>
-        <button className="btn btn-primary mx-2">-</button>
+        <button
+          className="btn btn-primary mx-2"
+          onClick={() => {
+            dispatch(actionCreators.withdrawMoney(10));
+          }}
+        >
+          -
+        </button>
         Update Balance
-        <button className="btn btn-primary mx-2">+</button>
+        <button
+          className="btn btn-primary mx-2"
+          onClick={() => {
+            dispatch(actionCreators.depositMoney(10));
+          }}
+        >
+          +
+        </button>
       </div>
     </>
   );
